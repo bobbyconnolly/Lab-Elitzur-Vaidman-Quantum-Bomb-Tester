b520ac18c7acff015705acd7db44cb88facc31f1
Thomas Connolly <connollythomas@hotmail.com>
2023-08-06 12:39:05 -0400

    add RenderableEntity

--- QuantumExperiment.cs
+++ QuantumExperiment.cs
@@ -13,6 +13,8 @@ public class QuantumExperiment
 
     private const double PHOTON_SIZE = 10;
 
+    // All positions correspond to the center point of the object
+
     private const double INITIAL_PHOTON_X = 50;
     private const double INITIAL_PHOTON_Y = 200;
 
@@ -22,17 +24,17 @@ public class QuantumExperiment
     private const double BEAM_SPLITTER_WIDTH = 5; 
     private const double BEAM_SPLITTER_HEIGHT = 20;
 
-    private const double BOMB_X = 300;
+    private const double BOMB_X = 350;
     private const double BOMB_Y = 200;
     private const double BOMB_SIZE = 20;
 
     private const double MIRROR1_X = 200;
     private const double MIRROR1_Y = 100;
 
-    private const double MIRROR2_X = 400;
+    private const double MIRROR2_X = 600;
     private const double MIRROR2_Y = 200;
 
-    private const double RECOMBINING_BEAM_SPLITTER_X = 400;
+    private const double RECOMBINING_BEAM_SPLITTER_X = 600;
     private const double RECOMBINING_BEAM_SPLITTER_Y = 100;
 
     private const double MIRROR_WIDTH = 5;
@@ -44,6 +46,77 @@ public class QuantumExperiment
     private event AnimationStep TopPhotonArrivesAtMirrorComplete;
     private event AnimationStep BottomPhotonArrivesAtMirrorComplete;
 
+    public class RenderableEntity
+    {
+        private Shape _shape;
+        private Canvas _canvas;
+
+        private double _centroidX;
+        private double _centroidY;
+
+        public Shape Shape
+        {
+            get => _shape;
+            private set
+            {
+                _shape = value;
+                UpdatePosition();
+            }
+        }
+
+        public double CentroidX
+        {
+            get => _centroidX;
+            set
+            {
+                _centroidX = value;
+                UpdatePosition();
+            }
+        }
+
+        public double CentroidY
+        {
+            get => _centroidY;
+            set
+            {
+                _centroidY = value;
+                UpdatePosition();
+            }
+        }
+
+        public RenderableEntity(Shape shape, Canvas canvas, double x, double y)
+        {
+            _shape = shape;
+            _canvas = canvas;
+            _centroidX = x;
+            _centroidY = y;
+
+            _canvas.Children.Add(_shape);
+            UpdatePosition();
+        }
+
+        public Point GetCentroid()
+        {
+            double x = Canvas.GetLeft(_shape) + _shape.Width / 2.0;
+            double y = Canvas.GetTop(_shape) + _shape.Height / 2.0;
+            return new Point(x, y);
+        }
+
+        private void UpdatePosition()
+        {
+            Canvas.SetLeft(_shape, _centroidX - _shape.Width / 2.0);
+            Canvas.SetTop(_shape, _centroidY - _shape.Height / 2.0);
+        }
+    }
+
+    public class Photon : RenderableEntity
+    {
+        public Photon(Shape shape, Canvas canvas, double x, double y) : base(shape, canvas, x, y)
+        {
+            // Additional Photon-specific initialization can go here, if needed.
+        }
+    }
+
     public QuantumExperiment(Canvas canvas)
     {
         _canvas = canvas;
@@ -51,21 +124,29 @@ public class QuantumExperiment
 
     public void Run()
     {
-        AddPhoton();
+        var photon = AddPhoton();
 
         AddBomb();
 
-        AddBeamSplitter();
+        var beamSplitter = AddBeamSplitter(new Point(BEAM_SPLITTER_X, BEAM_SPLITTER_Y));
+        AddBeamSplitter(new Point(RECOMBINING_BEAM_SPLITTER_X, RECOMBINING_BEAM_SPLITTER_Y));
 
-        AddRecombiningBeamSplitter();
+        AddMirror(new Point(MIRROR1_X, MIRROR1_Y));
+        AddMirror(new Point(MIRROR2_X, MIRROR2_Y));
 
-        AddMirror(MIRROR1_X, MIRROR1_Y);
-        AddMirror(MIRROR2_X, MIRROR2_Y);
-
-        //AnimatePhoton(photon, 400, 1000);
+        MovePhotonToBeamSplitter(photon, beamSplitter, () =>
+        {
+            Debug.WriteLine("Photon reached the beam splitter!");
+            // Place any additional logic you want to execute here.
+        });
     }
 
-    private void AddPhoton()
+    public void MovePhotonToBeamSplitter(Photon photon, RenderableEntity beamSplitter, Action callback)
+    {
+        AnimatePhoton(photon, beamSplitter.GetCentroid(), 1000, callback);
+    }
+
+    private Photon AddPhoton()
     {
         Ellipse photon = new Ellipse
         {
@@ -74,13 +155,10 @@ public class QuantumExperiment
             Fill = Brushes.Magenta
         };
 
-        _canvas.Children.Add(photon);
-
-        Canvas.SetLeft(photon, INITIAL_PHOTON_X - PHOTON_SIZE / 2.0);
-        Canvas.SetTop(photon, INITIAL_PHOTON_Y - PHOTON_SIZE / 2.0);
+        return new Photon(photon, _canvas, INITIAL_PHOTON_X, INITIAL_PHOTON_Y);
     }
 
-    private void AddBomb()
+    private RenderableEntity AddBomb()
     {
         Ellipse bomb = new Ellipse
         {
@@ -89,13 +167,10 @@ public class QuantumExperiment
             Fill = Brushes.Black
         };
 
-        _canvas.Children.Add(bomb);
-
-        Canvas.SetLeft(bomb, BOMB_X - BOMB_SIZE / 2.0);
-        Canvas.SetTop(bomb, BOMB_Y - BOMB_SIZE / 2.0);
+        return new RenderableEntity(bomb, _canvas, BOMB_X, BOMB_Y);
     }
 
-    private void AddBeamSplitter()
+    private RenderableEntity AddBeamSplitter(Point centroid)
     {
         Rectangle beamSplitter = new Rectangle
         {
@@ -104,38 +179,14 @@ public class QuantumExperiment
             Fill = Brushes.LightGreen
         };
 
-        // Set the beam splitter's position
-        Canvas.SetLeft(beamSplitter, BEAM_SPLITTER_X - BEAM_SPLITTER_WIDTH / 2); 
-        Canvas.SetTop(beamSplitter, BEAM_SPLITTER_Y - BEAM_SPLITTER_HEIGHT / 2);
-
         // Rotate the beam splitter
-        RotateTransform rotateTransform = new RotateTransform(45.0, BEAM_SPLITTER_WIDTH / 2, BEAM_SPLITTER_HEIGHT / 2);
+        RotateTransform rotateTransform = new RotateTransform(45.0, BEAM_SPLITTER_WIDTH / 2.0, BEAM_SPLITTER_HEIGHT / 2.0);
         beamSplitter.RenderTransform = rotateTransform;
 
-        _canvas.Children.Add(beamSplitter);
+        return new RenderableEntity(beamSplitter, _canvas, centroid.X, centroid.Y);
     }
 
-    private void AddRecombiningBeamSplitter()
-    {
-        Rectangle recombiningBeamSplitter = new Rectangle
-        {
-            Width = BEAM_SPLITTER_WIDTH,
-            Height = BEAM_SPLITTER_HEIGHT,
-            Fill = Brushes.LightGreen
-        };
-
-        // Set the beam splitter's position
-        Canvas.SetLeft(recombiningBeamSplitter, RECOMBINING_BEAM_SPLITTER_X - BEAM_SPLITTER_WIDTH / 2);
-        Canvas.SetTop(recombiningBeamSplitter, RECOMBINING_BEAM_SPLITTER_Y - BEAM_SPLITTER_HEIGHT / 2);
-
-        // Rotate the beam splitter
-        RotateTransform rotateTransform = new RotateTransform(45.0, BEAM_SPLITTER_WIDTH / 2, BEAM_SPLITTER_HEIGHT / 2);
-        recombiningBeamSplitter.RenderTransform = rotateTransform;
-
-        _canvas.Children.Add(recombiningBeamSplitter);
-    }
-
-    private void AddMirror(double mirrorX, double mirrorY)
+    private RenderableEntity AddMirror(Point centroid)
     {
         Rectangle mirror = new Rectangle
         {
@@ -144,18 +195,14 @@ public class QuantumExperiment
             Fill = Brushes.MediumPurple
         };
 
-        // Set the beam splitter's position
-        Canvas.SetLeft(mirror, mirrorX - MIRROR_WIDTH / 2);
-        Canvas.SetTop(mirror, mirrorY - MIRROR_HEIGHT / 2);
-
-        // Rotate the beam splitter
-        RotateTransform rotateTransform = new RotateTransform(45.0, MIRROR_WIDTH / 2, MIRROR_HEIGHT / 2);
+        // Rotate the mirror
+        RotateTransform rotateTransform = new RotateTransform(45.0, MIRROR_WIDTH / 2.0, MIRROR_HEIGHT / 2.0);
         mirror.RenderTransform = rotateTransform;
 
-        _canvas.Children.Add(mirror);
+        return new RenderableEntity(mirror, _canvas, centroid.X, centroid.Y);
     }
 
-    public void AnimatePhoton(Ellipse photon, double targetX, double durationMilliseconds)
+    public void AnimatePhoton(Photon photon, Point targetPosition, double durationMilliseconds, Action onComplete = null)
     {
         // This timer will fire every frame
         var timer = new DispatcherTimer
@@ -163,10 +210,10 @@ public class QuantumExperiment
             Interval = TimeSpan.FromMilliseconds(1000.0 / 60.0) // 60 FPS
         };
 
-        double startX = Canvas.GetLeft(photon);
-        double distance = targetX - startX;
+        Point startPosition = photon.GetCentroid();
+
         var stopwatch = new Stopwatch();
-         
+
         // Create a new Polyline for the photon's path (dashed line)
         var pathDashed = new Polyline
         {
@@ -180,13 +227,16 @@ public class QuantumExperiment
         {
             // Determine how far along the duration we are (clamped to 1)
             double progress = Math.Min(1.0, stopwatch.ElapsedMilliseconds / durationMilliseconds);
-            double newX = startX + distance * progress;
+
+            double newX = startPosition.X + (targetPosition.X - startPosition.X) * progress;
+            double newY = startPosition.Y + (targetPosition.Y - startPosition.Y) * progress;
 
             // Move the photon to the current position
-            Canvas.SetLeft(photon, newX);
+            photon.CentroidX = newX;
+            photon.CentroidY = newY;
 
             // Add a new point to the Polyline at the photon's current position
-            var newPoint = new Point(newX, Canvas.GetTop(photon) + photon.Height / 2);
+            var newPoint = new Point(newX, newY);
             pathDashed.Points.Add(newPoint);
 
             // Stop the timer and the animation when we're done
@@ -194,6 +244,8 @@ public class QuantumExperiment
             {
                 ((DispatcherTimer)sender).Stop();
                 stopwatch.Stop();
+
+                onComplete?.Invoke(); // Notify that the animation is complete
             }
         };
 
